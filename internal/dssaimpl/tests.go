1bee7fa6e5b41c20da0157ae98cb6d84bf13677f
thierry_otvl <tbeigbeder.nospam@gmail.com>
2026-09-09 13:48:20 +0000

    prepare plugin testing framework

---
+++ internal/dssaimpl/tests.go
@@ -0,0 +1,52 @@
+package dssaimpl
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/t-beigbeder/vdasync/dssa"
+	"github.com/t-beigbeder/vdasync/internal/dssaimpl/localfiles"
+)
+
+type TestDss struct {
+	kind string
+	dss  dssa.Dssa
+}
+
+type TestDssOptions struct {
+	Kind          string // "lf", "sftp"
+	SftpUser      string
+	SftpHost      string
+	SftpPort      string
+	SftpIdent     string
+	SftpRoot      string
+	SftpKHFile    string
+	SftpHasServer bool
+}
+
+func NewTestDss(tdo *TestDssOptions) (*TestDss, error) {
+	var (
+		err     error
+		testDss *TestDss
+	)
+	switch tdo.Kind {
+	case "lf":
+		testDss = &TestDss{dss: localfiles.MakeLocalFilesDssa()}
+	default:
+		err = fmt.Errorf("kind for TestDssOptions %s unknown", tdo.Kind)
+	}
+	if err != nil {
+		return nil, err
+	}
+	testDss.kind = tdo.Kind
+	return testDss, nil
+}
+
+func (testDss *TestDss) Close() error {
+	switch testDss.kind {
+	case "lf":
+		return nil
+	default:
+		return fmt.Errorf("TestDss.Close: kind %s unknown", testDss.kind)
+	}
+}
---
+++ internal/dssaimpl/tests_test.go
@@ -0,0 +1,7 @@
+package dssaimpl
+
+import "testing"
+
+func TestAll(t *testing.T) {
+
+}
